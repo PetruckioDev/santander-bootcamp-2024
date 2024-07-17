5ec486ea0be521034f2fd71e67b6dd88591da83d
petruckioDev <mauriciofpferreira@gmail.com>
2024-07-16 21:17:34 -0400

    feat(entities): add database entity classes

---
+++ src/main/java/com/petruckiodev/santander_bootcamp_2024/model/Feature.java
@@ -0,0 +1,7 @@
+package com.petruckiodev.santander_bootcamp_2024.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+}
---
+++ src/main/java/com/petruckiodev/santander_bootcamp_2024/model/News.java
@@ -0,0 +1,7 @@
+package com.petruckiodev.santander_bootcamp_2024.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem {
+}
