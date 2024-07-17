43cc58d49aa7b16c3f7b24af2cba195ef903f0ae
petruckioDev <mauriciofpferreira@gmail.com>
2024-07-16 21:18:49 -0400

    feat(user-repo): add JPA interface for UserRepository.java

---
+++ src/main/java/com/petruckiodev/santander_bootcamp_2024/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.petruckiodev.santander_bootcamp_2024.repository;
+
+import com.petruckiodev.santander_bootcamp_2024.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
